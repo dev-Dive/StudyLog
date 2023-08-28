e6c8542c39dcd84b28d62207766bf880962c12a0
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-08-28 23:51:22 +0900

    feat: 메일 컨텐츠 생성
    SL-111

--- backend/src/main/java/com/devdive/backend/auth/application/service/SendMailService.java
+++ backend/src/main/java/com/devdive/backend/auth/application/service/SendMailService.java
@@ -1,6 +1,7 @@
 package com.devdive.backend.auth.application.service;
 
 import com.devdive.backend.auth.application.port.in.SendMailUseCase;
+import com.devdive.backend.auth.application.port.out.persistence.LoadMemberPort;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ public class SendMailService implements SendMailUseCase {
 
     private final JavaMailSender sender;
     private final MailContentGenerator generator;
+    private final LoadMemberPort loadMemberPort;
 
     @Value("${spring.mail.username}")
     private String fromAddress;
@@ -30,5 +32,21 @@ public class SendMailService implements SendMailUseCase {
         helper.setText(generator.generateContent(), true);
         helper.setTo(toMail);
         sender.send(message);
+    }
+
+    private void setMailContent(String toMail, MimeMessageHelper helper) throws MessagingException {
+        if (loadMemberPort.isRegisteredMail(toMail)) {
+            setLoginMail(toMail, helper);
+            return;
+        }
+        setRegisterMail(toMail, helper);
+    }
+
+    private void setLoginMail(String toMail, MimeMessageHelper helper) throws MessagingException {
+        helper.setText(generator.generateAuthContent(toMail, "register"), true);
+    }
+
+    private void setRegisterMail(String toMail, MimeMessageHelper helper) throws MessagingException {
+        helper.setText(generator.generateAuthContent(toMail, "login"), true);
     }
 }
