17b7dd1f964eea12af8cbb43e2b505bd50329131
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-08-28 23:51:22 +0900

    feat: 메일 컨텐츠 생성
    SL-111

--- backend/src/main/java/com/devdive/backend/auth/application/service/MailContentGenerator.java
+++ backend/src/main/java/com/devdive/backend/auth/application/service/MailContentGenerator.java
@@ -1,20 +1,36 @@
 package com.devdive.backend.auth.application.service;
 
+import com.devdive.backend.auth.application.service.jwt.JwtProperties;
+import com.devdive.backend.auth.application.service.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @RequiredArgsConstructor
 public class MailContentGenerator {
 
-    private String subject = "default subject";
-    private String text = "default text";
+    private final String MAIL_FORMAT = "<button onclick=\'\"window.open(\'%s\')\">button</button> <br/> %s";
+
+    private final String subject = "StudyLog 운영팀";
 
     public String generateSubject() {
         return subject;
     }
 
-    public String generateContent() {
-        return text;
+    public String generateAuthContent(String mail, String request) {
+        JwtProvider jwtProvider = new JwtProvider(new JwtProperties(1));
+        String jwt = jwtProvider.createJwtToken(mail);
+
+        String uri = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host("www.dev-dive.com")
+                .path("/api/v1/")
+                .path(request)
+                .queryParam("token", jwt)
+                .build(true)
+                .toUriString();
+
+        return String.format(MAIL_FORMAT, uri, uri);
     }
 }
