09b0be32009449b947363d12bf4252b4b4521bfc
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-09-16 21:14:35 +0900

    test: 스터디원은 스터디글을 작성 후 저장할 수 있다
    SL-164

---
+++ backend/src/test/java/com/devdive/backend/acceptance/PostAcceptanceTest.java
@@ -0,0 +1,40 @@
+package com.devdive.backend.acceptance;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class PostAcceptanceTest extends AcceptanceTest {
+    @Test
+    @DisplayName("스터디원은 스터디글을 작성 후 저장할 수 있다")
+    public void member_allows_to_write_And_save_Post () throws JSONException {
+        // given
+        JSONObject request = new JSONObject();
+        request.put("studyId",1L);
+        request.put("thumbnailUrl","http://");
+        request.put("title","title");
+        request.put("subtitle","subtitle");
+        request.put("content","content");
+        request.put("tags", List.of("tag1", "tag2"));
+
+        // when, then
+        given()
+                .log().all()
+                .body(request.toString())
+               .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .when()
+               .post("/v1/posts")
+        .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
