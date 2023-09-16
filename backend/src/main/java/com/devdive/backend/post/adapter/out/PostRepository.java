0377770fcb0db523e6049e36ee07c0434de41105
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-09-16 22:22:36 +0900

    feat: 글 저장 기능 구현
    SL-164

---
+++ backend/src/main/java/com/devdive/backend/post/adapter/out/PostRepository.java
@@ -0,0 +1,7 @@
+package com.devdive.backend.post.adapter.out;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<PostJpaEntity, Long> {
+
+}
