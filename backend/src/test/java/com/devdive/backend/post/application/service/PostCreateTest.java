042db30dc7221ff1367bf28876d6ac5f2dfa8620
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-09-16 21:42:51 +0900

    refactor: DTO 패키지 생성 및 이동
    SL-164

---
+++ backend/src/test/java/com/devdive/backend/post/application/service/PostCreateTest.java
@@ -0,0 +1,12 @@
+package com.devdive.backend.post.application.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PostCreateTest {
+
+    @Test
+    @DisplayName("글 생성")
+    public void createPost(){
+    }
+}
