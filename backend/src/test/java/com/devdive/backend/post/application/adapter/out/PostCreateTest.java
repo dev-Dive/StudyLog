7c592b37aa2dd1178bad55fbde3f4f2a95b34062
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-09-16 22:21:42 +0900

    test: 글 저장 테스트 작성
    SL-164

---
+++ backend/src/test/java/com/devdive/backend/post/application/adapter/out/PostCreateTest.java
@@ -0,0 +1,49 @@
+package com.devdive.backend.post.application.adapter.out;
+
+import com.devdive.backend.post.adapter.out.PostJpaEntity;
+import com.devdive.backend.post.adapter.out.PostPersistenceAdapter;
+import com.devdive.backend.post.adapter.out.PostRepository;
+import com.devdive.backend.post.application.dto.PostCreateRequestDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class PostCreateTest {
+
+    @Autowired
+    PostPersistenceAdapter adapter;
+
+    @Autowired
+    PostRepository repository;
+
+    @Test
+    @DisplayName("글 생성")
+    public void creatPost(){
+        PostCreateRequestDto dto = new PostCreateRequestDto(
+                1L,
+                "http://",
+                "title",
+                "sub",
+                "content",
+                List.of("tag1", "tag2")
+        );
+
+        adapter.createPost(dto);
+
+        PostJpaEntity saved = repository.findById(1L).get();
+
+        assertEquals(1L, saved.getStudyId());
+        assertEquals("http://", saved.getThumbnail_url());
+        assertEquals("title", saved.getTitle());
+        assertEquals("sub", saved.getSubtitle());
+        assertEquals("content", saved.getContent());
+    }
+}
