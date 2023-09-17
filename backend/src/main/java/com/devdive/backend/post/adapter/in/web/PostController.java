1a413e1079f1f3a6553e4f70689e88f56177c964
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-09-18 00:09:40 +0900

    test: 글 저장 기능 구현 web
    SL-164

--- backend/src/main/java/com/devdive/backend/post/adapter/in/web/PostController.java
+++ backend/src/main/java/com/devdive/backend/post/adapter/in/web/PostController.java
@@ -1,7 +1,9 @@
 package com.devdive.backend.post.adapter.in.web;
 
 import com.devdive.backend.post.application.dto.PostCreateRequestDto;
+import com.devdive.backend.post.application.port.in.PostUseCase;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostUseCase useCase;
 
     @PostMapping("/v1/posts")
     ResponseEntity<Void> sendMail(@RequestBody @Valid PostCreateRequestDto dto) {
+        useCase.createPost(dto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
