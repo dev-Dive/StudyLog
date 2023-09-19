8c675f541475d3191c00324e8beded334fcc3c15
Daeeun Ko <7273322@naver.com>
2023-09-19 17:41:02 +0900

    chore: 스키마 작성
    SL-173

---
+++ backend/src/main/resources/db/schema.sql
@@ -0,0 +1,63 @@
+CREATE TABLE `members`
+(
+    `id`          BIGINT       NOT NULL,
+    `name`        VARCHAR(255) NOT NULL,
+    `mail`        VARCHAR(255) NOT NULL,
+    `profile_url` VARCHAR(255) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE `studies`
+(
+    `id`          BIGINT       NOT NULL,
+    `name`        VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `profile_url` VARCHAR(255) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE `posts`
+(
+    `id`            BIGINT       NOT NULL,
+    `content`       LONGTEXT     NOT NULL,
+    `thumbnail_url` VARCHAR(255) NOT NULL,
+    `title`         VARCHAR(255) NOT NULL,
+    `subtitle`      VARCHAR(255) NOT NULL,
+    `study_id`      BIGINT       NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE `tags`
+(
+    `id`   BIGINT       NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE `study_members`
+(
+    `member_id` BIGINT       NOT NULL,
+    `study_id`  BIGINT       NOT NULL,
+    `role`      VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `post_authors`
+(
+    `member_id` BIGINT NOT NULL,
+    `post_id`   BIGINT NOT NULL
+);
+
+CREATE TABLE `post_tags`
+(
+    `post_id` BIGINT NOT NULL,
+    `tag_id`  BIGINT NOT NULL
+);
+
+ALTER TABLE `study_members` ADD FOREIGN KEY (`member_id`) REFERENCES members(id);
+ALTER TABLE `study_members` ADD FOREIGN KEY (`study_id`) REFERENCES studies(id);
+
+ALTER TABLE `post_authors` ADD FOREIGN KEY (`member_id`) REFERENCES members(id);
+ALTER TABLE `post_authors` ADD FOREIGN KEY (`post_id`) REFERENCES posts(id);
+
+ALTER TABLE `post_tags` ADD FOREIGN KEY (`post_id`) REFERENCES posts(id);
+ALTER TABLE `post_tags` ADD FOREIGN KEY (`tag_id`) REFERENCES tags(id);
