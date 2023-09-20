2f9b12aff7879582c358ad768870ed1a45fdd79d
hunko <rhwlgns4386@naver.com>
2023-09-20 20:16:45 +0900

    test: 인가 처리 매니저 테스트 작성
    SL-170

---
+++ backend/src/test/java/com/devdive/backend/security/access/manager/AccessDecisionManagerImpTest.java
@@ -0,0 +1,73 @@
+package com.devdive.backend.security.access.manager;
+
+import com.devdive.backend.security.access.AccessDecisionVoter;
+import com.devdive.backend.security.core.Authentication;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class AccessDecisionManagerImpTest {
+
+    @Test
+    @DisplayName("권한 인증 성공")
+    void authorizeSuccess() throws AccessDeniedException {
+
+        AccessDecisionManagerImp accessDecisionManager =
+                new AccessDecisionManagerImp(List.of((authentication, roles) -> {
+                    String credentials = (String)authentication.getCredentials();
+                    return credentials.equals(roles.get(0))?AccessDecisionVoter.ACCESS_GRANTED:AccessDecisionVoter.ACCESS_DENIED;
+                }));
+
+        accessDecisionManager.decide(new Authentication() {
+            @Override
+            public Object getCredentials() {
+                return "ADMIN";
+            }
+
+            @Override
+            public Object getDetails() {
+                return null;
+            }
+
+            @Override
+            public Object getPrincipal() {
+                return null;
+            }
+        },List.of("ADMIN"));
+    }
+
+    @Test
+    @DisplayName("권한 인증 실패")
+    void authorizeFail() {
+
+        AccessDecisionManagerImp accessDecisionManager =
+                new AccessDecisionManagerImp(List.of((authentication, roles) -> {
+                    String credentials = (String)authentication.getCredentials();
+                    return credentials.equals(roles.get(0))?AccessDecisionVoter.ACCESS_GRANTED:AccessDecisionVoter.ACCESS_DENIED;
+                }));
+
+        Authentication authentication = new Authentication() {
+            @Override
+            public Object getCredentials() {
+                return "USER";
+            }
+
+            @Override
+            public Object getDetails() {
+                return null;
+            }
+
+            @Override
+            public Object getPrincipal() {
+                return null;
+            }
+        };
+
+        assertThatThrownBy(()->accessDecisionManager.decide(authentication,List.of("ADMIN")))
+                .isInstanceOf(AccessDeniedException.class);
+    }
+}
