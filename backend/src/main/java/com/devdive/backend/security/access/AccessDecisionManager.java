a6232dd7d931e4c16c2474b388fd82612230d7eb
hunko <rhwlgns4386@naver.com>
2023-09-20 20:18:33 +0900

    feat: 인가 처리 매니저 구현
    SL-170 인가 처리를 허가 할지에 관한 결정을 한다. 만약 인가처리를 허가 하지 않는다면 AccessDeniedException을 발생시킨다.

---
+++ backend/src/main/java/com/devdive/backend/security/access/AccessDecisionManager.java
@@ -0,0 +1,10 @@
+package com.devdive.backend.security.access;
+
+import com.devdive.backend.security.core.Authentication;
+
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+
+public interface AccessDecisionManager {
+    void decide(Authentication authentication, List<String> roles) throws AccessDeniedException;
+}
