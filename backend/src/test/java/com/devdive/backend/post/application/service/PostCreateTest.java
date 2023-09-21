8cb128bbdd8617a7caac80dfa1e6a28a33342edb
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-09-21 22:18:03 +0900

    test: 글 조회 테스트 생성
    SL-164

--- backend/src/test/java/com/devdive/backend/post/application/service/PostCreateTest.java
+++ backend/src/test/java/com/devdive/backend/post/application/service/PostCreateTest.java
@@ -1,6 +1,7 @@
 package com.devdive.backend.post.application.service;
 
 import com.devdive.backend.post.application.dto.PostCreateRequestDto;
+import com.devdive.backend.post.application.dto.PostViewDto;
 import com.devdive.backend.post.application.port.out.persistence.LoadPostPort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,11 +9,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.opentest4j.AssertionFailedError;
 
 import java.util.List;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PostCreateTest {
@@ -42,5 +45,52 @@ public class PostCreateTest {
 
         // then
         verify(loadPostPort).createPost(dto);
+    }
+
+    @Test
+    @DisplayName("글 조회")
+    public void viewPost(){
+        // given
+        long postId = 1L;
+        PostViewDto postViewDto = new PostViewDto(
+                postId,
+                1L,
+                "http://",
+                "title",
+                "sub",
+                "content",
+                List.of("tag1", "tag2"));
+        when(loadPostPort.viewPost(postId)).thenReturn(postViewDto);
+
+        // when
+        loadPostPort.viewPost(postId);
+
+        // then
+        assertThat(postId).isEqualTo(postViewDto.getId());
+    }
+
+    @Test
+    @DisplayName("글 조회 실패")
+    public void viewPostFail(){
+        // given
+        long postId = 1L;
+        PostViewDto postViewDto = new PostViewDto(
+                postId,
+                1L,
+                "http://",
+                "title",
+                "sub",
+                "content",
+                List.of("tag1", "tag2"));
+        when(loadPostPort.viewPost(postId)).thenReturn(postViewDto);
+
+        // when
+        loadPostPort.viewPost(postId);
+
+        // then
+        assertThrows(AssertionFailedError.class, () -> {
+            assertThat(postId).isEqualTo(postViewDto.getId() + 1);
+        });
+
     }
 }
