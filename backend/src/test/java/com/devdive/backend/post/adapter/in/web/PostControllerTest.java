cef4a1ce43ef3590a3e8e378f96d1116b42cfc75
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-09-21 22:18:03 +0900

    test: 글 조회 테스트 생성
    SL-164

--- backend/src/test/java/com/devdive/backend/post/adapter/in/web/PostControllerTest.java
+++ backend/src/test/java/com/devdive/backend/post/adapter/in/web/PostControllerTest.java
@@ -1,6 +1,7 @@
 package com.devdive.backend.post.adapter.in.web;
 
 import com.devdive.backend.post.application.dto.PostCreateRequestDto;
+import com.devdive.backend.post.application.dto.PostViewDto;
 import com.devdive.backend.post.application.port.in.PostUseCase;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.List;
 
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = PostController.class)
@@ -50,6 +52,30 @@ class PostControllerTest {
                 .contentType(MediaType.APPLICATION_JSON);
 
         // then
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("글 조회 테스트")
+    public void viewPost() throws Exception {
+        // given
+        long postId = 1L;
+
+        PostViewDto postViewDto = new PostViewDto(
+                postId,
+                1L,
+                "http://",
+                "title",
+                "sub",
+                "content",
+                List.of("tag1", "tag2"));
+        when(useCase.viewPost(postId)).thenReturn(postViewDto);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/v1/posts/" + postId)
+                .contentType(MediaType.APPLICATION_JSON);
+
         mockMvc.perform(request)
                 .andExpect(status().isOk());
     }
