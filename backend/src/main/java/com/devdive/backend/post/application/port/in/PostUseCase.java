b95687bf4f593b104e708aaec2f4fa64201a4c33
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-09-21 22:19:04 +0900

    feat: 글 조회 application 구현
    SL-164

--- backend/src/main/java/com/devdive/backend/post/application/port/in/PostUseCase.java
+++ backend/src/main/java/com/devdive/backend/post/application/port/in/PostUseCase.java
@@ -1,8 +1,11 @@
 package com.devdive.backend.post.application.port.in;
 
 import com.devdive.backend.post.application.dto.PostCreateRequestDto;
+import com.devdive.backend.post.application.dto.PostViewDto;
 
 public interface PostUseCase {
 
     void createPost(PostCreateRequestDto dto);
+
+    PostViewDto viewPost(Long postId);
 }
