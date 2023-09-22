438981d1b31862f119b604053b0002ee3ba193bd
y00eunji <aine0227@naver.com>
2023-09-22 16:10:41 +0900

    feat: 로그인 및 회원가입 구현(url)
    SL-167

--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/?type=login',
+        permanent: false,
+      },
+      {
+        source: '/register',
+        destination: '/?type=register',
+        permanent: false,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
