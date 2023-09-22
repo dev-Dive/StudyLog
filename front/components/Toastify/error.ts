185a2cddbba495dc8ccdf013e4441d224a213b40
y00eunji <aine0227@naver.com>
2023-09-22 16:08:34 +0900

    feat: toastify 성공 및 에러 메세지
    SL-167

---
+++ front/components/Toastify/error.ts
@@ -0,0 +1,15 @@
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export const notifyError = (text: string) => {
+  toast.error(`${text}`, {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'colored',
+  })
+}
