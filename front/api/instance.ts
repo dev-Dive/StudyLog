ab31a02abf30643cf4080472ebbcd5984f9d31c2
y00eunji <aine0227@naver.com>
2023-09-22 16:06:21 +0900

    feat: API 인스턴스 생성 및 요청
    SL-167

---
+++ front/api/instance.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export const Api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  withCredentials: true,
+})
+
+export const AuthApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  withCredentials: true,
+})
+
+AuthApi.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken')
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
