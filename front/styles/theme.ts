57fd5e3e413c79ff0c92b28d2c2255cad84bb83d
y00eunji <aine0227@naver.com>
2023-09-22 16:09:44 +0900

    design: 사용하는 컬러 객체
    SL-167

---
+++ front/styles/theme.ts
@@ -0,0 +1,18 @@
+const color = {
+  gray1: '#EFEFEF',
+  gray2: '#F6F6F6',
+  gray3: '#EDEDED',
+  gray4: '#E1E1E1',
+  gray5: '#666666',
+  white: '#FFFFFF',
+  mainColor: '#5A6DD5',
+  lightMain1: '#A5A5DB',
+  lightMain2: '#EFEDFF',
+  black: '#000000',
+  warnInside: '#FFB0B0',
+  warnOutSide: '#FF4242',
+  webBack: '#F4F4F4',
+  red: '#FF0000',
+}
+
+export default color
