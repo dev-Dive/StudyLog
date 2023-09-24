49db95ca470362a9bc2b3174c26799299d55a377
hunko <rhwlgns4386@naver.com>
2023-09-24 18:54:27 +0900

    feat: 필터들을 정렬해주는 Comparator 구현
    SL-170

---
+++ backend/src/main/java/com/devdive/backend/security/config/builder/FilterComparator.java
@@ -0,0 +1,50 @@
+package com.devdive.backend.security.config.builder;
+
+import com.devdive.backend.security.filter.AnonymousAuthenticationFilter;
+import com.devdive.backend.security.filter.ExceptionTranslationFilter;
+import com.devdive.backend.security.filter.FilterSecurityInterceptor;
+import com.devdive.backend.security.filter.SecurityContextPersistenceFilter;
+import jakarta.servlet.Filter;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FilterComparator implements Comparator<Filter> {
+
+    private final Map<String,Integer> filterToOrder=new HashMap<>();
+    private static final int INITIAL_ORDER = 100;
+    private static final int ORDER_STEP = 100;
+
+    public FilterComparator() {
+        Step step = new Step(INITIAL_ORDER, ORDER_STEP);
+        filterToOrder.put(SecurityContextPersistenceFilter.class.getName(),step.next());
+        filterToOrder.put(AnonymousAuthenticationFilter.class.getName(),step.next());
+        filterToOrder.put(ExceptionTranslationFilter.class.getName(),step.next());
+        filterToOrder.put(FilterSecurityInterceptor.class.getName(),step.next());
+    }
+
+    @Override
+    public int compare(Filter o1, Filter o2) {
+        Integer left = filterToOrder.get(o1.getClass().getName());
+        Integer right = filterToOrder.get(o2.getClass().getName());
+        return left-right;
+    }
+
+    private static class Step {
+
+        private int value;
+        private final int stepSize;
+
+        Step(int initialValue, int stepSize) {
+            this.value = initialValue;
+            this.stepSize = stepSize;
+        }
+
+        int next() {
+            int value = this.value;
+            this.value += this.stepSize;
+            return value;
+        }
+    }
+}
