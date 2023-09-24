d143e82ec04b4e06b9a954a3a7e5f810bd50af9d
hunko <rhwlgns4386@naver.com>
2023-09-24 18:48:17 +0900

    feat: SecurityContextPersistenceFilter 기능 구현
    SL-170

---
+++ backend/src/main/java/com/devdive/backend/security/filter/SecurityContextPersistenceFilter.java
@@ -0,0 +1,16 @@
+package com.devdive.backend.security.filter;
+
+import com.devdive.backend.security.core.SecurityContextHolder;
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class SecurityContextPersistenceFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        SecurityContextHolder.addContext(SecurityContextHolder.createEmptyContext());
+        chain.doFilter(request,response);
+        SecurityContextHolder.clearContext();
+    }
+}
