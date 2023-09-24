fe3925542e1aec40e8d4a9b7ae8712eac4cc37d3
hunko <rhwlgns4386@naver.com>
2023-09-24 18:45:34 +0900

    test: SecurityContextHolderStrategy 구현체 테스트 작성
    SL-170

---
+++ backend/src/test/java/com/devdive/backend/security/core/SecurityContextHolderStrategyImpTest.java
@@ -0,0 +1,44 @@
+package com.devdive.backend.security.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class SecurityContextHolderStrategyImpTest {
+
+    private final SecurityContextHolderStrategy strategy=new SecurityContextHolderStrategyImp();
+
+    @BeforeEach
+    void before(){
+        strategy.clearContext();
+    }
+
+    @Test
+    void getContext() {
+        SecurityContextImp securityContextImp = new SecurityContextImp();
+        strategy.setContext(securityContextImp);
+        assertThat(strategy.getContext()).isEqualTo(securityContextImp);
+    }
+
+    @Test
+    void setContext() {
+        SecurityContextImp securityContextImp = new SecurityContextImp();
+        assertThat(strategy.getContext()).isNotEqualTo(securityContextImp);
+        strategy.setContext(securityContextImp);
+        assertThat(strategy.getContext()).isEqualTo(securityContextImp);
+    }
+
+    @Test
+    void createEmptyContext() {
+        assertThat(strategy.createEmptyContext()).isNotNull();
+    }
+
+    @Test
+    void clearContext() {
+        SecurityContextImp securityContext = new SecurityContextImp();
+        strategy.setContext(securityContext);
+
+        strategy.clearContext();
+        assertThat(strategy.getContext()).isNotEqualTo(securityContext);
+    }
+}
