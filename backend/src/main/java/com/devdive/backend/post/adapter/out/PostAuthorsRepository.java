57473b762f32172dd67717005f11e68816a92d9d
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-10-03 18:29:01 +0900

    feat: delete 기능 삭제
    SL-164

--- backend/src/main/java/com/devdive/backend/post/adapter/out/PostAuthorsRepository.java
+++ backend/src/main/java/com/devdive/backend/post/adapter/out/PostAuthorsRepository.java
@@ -3,5 +3,4 @@ package com.devdive.backend.post.adapter.out;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostAuthorsRepository extends JpaRepository<PostAuthorsEntity, Long> {
-    void deleteByPostId(long postId);
 }
