fc25fcd6098fa049d82678fc66708072dbbf849b
y00eunji <aine0227@naver.com>
2023-10-03 14:36:58 +0900

    feat: TextArea구현 및 스타일링
    SL-122

---
+++ front/components/Write/ContextTextArea.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled'
+import { color } from '@/styles/theme'
+
+interface ContextTextAreaProps {
+  content: string
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+}
+
+export default function InputContextTextArea({
+  content,
+  onChange,
+}: ContextTextAreaProps): JSX.Element {
+  return (
+    <StyledTextArea
+      rows={10}
+      cols={50}
+      onChange={onChange}
+      value={content}
+      placeholder="내용을 입력해주세요"
+    />
+  )
+}
+
+const StyledTextArea = styled.textarea`
+  width: 100%;
+
+  border: 0;
+  resize: none;
+  font-size: 1.2rem;
+  flex: 1;
+
+  outline: none;
+
+  ::-webkit-scrollbar-track {
+    background-color: none;
+  }
+  ::-webkit-scrollbar {
+    width: 5px;
+    background-color: none;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${color.mainColor};
+    border-radius: 1rem;
+  }
+`
