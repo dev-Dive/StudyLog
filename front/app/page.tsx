423daedc87284a5a5581433d84478bf997caf92d
y00eunji <aine0227@naver.com>
2023-10-03 14:17:07 +0900

    feat: 100vh시 스크롤 방지
    Sl-100

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -3,9 +3,14 @@ import styles from './page.module.css'
 import Main from '@/components/Main/Main'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Main />
-    </main>
-  )
+  let vh = window.innerHeight * 0.01
+
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  })
+
+  return <Main />
 }
