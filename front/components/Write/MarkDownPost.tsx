362fbf371d1fc041752a43f38c29a60763613613
y00eunji <aine0227@naver.com>
2023-10-03 14:37:40 +0900

    feat: 마크다운 문법 설정 및 스타일링
    SL-122

---
+++ front/components/Write/MarkDownPost.tsx
@@ -0,0 +1,80 @@
+import Markdown from 'react-markdown'
+import styled from '@emotion/styled'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import remarkGfm from 'remark-gfm'
+import Image from 'next/image'
+
+type postType = {
+  content: string
+}
+
+export default function MarkDownPost({ content }: postType) {
+  return (
+    <div>
+      <style>{tableStyles}</style>
+      <style>{blockQuoteStyles}</style>
+      <Markdown
+        remarkPlugins={[remarkGfm]} // Allows us to have embedded HTML tags in our markdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                language={match[1]}
+                PreTag="div"
+                {...props}
+                style={materialDark}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...props}>{children}</code>
+            )
+          },
+          img: (image) => (
+            <StyledMarkDownImg
+              src={image.src || ''}
+              alt={image.alt || ''}
+              width={500}
+              height={300}
+            />
+          ),
+          table: ({ node, ...props }) => (
+            <table style={{ border: '1px solid black' }} {...props} />
+          ),
+        }}
+      >
+        {content}
+      </Markdown>
+    </div>
+  )
+}
+
+const StyledMarkDownImg = styled.img`
+  width: 100%;
+  height: auto;
+  max-height: 300px;
+  object-fit: contain;
+`
+
+const tableStyles = `
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+th, td {
+  border: 1px solid black;
+  padding: 8px;
+  text-align: left;
+}
+`
+
+const blockQuoteStyles = `
+blockquote {
+  color: #666;
+  margin: 0;
+  padding-left: 3em;
+  border-left: 0.5em #eee solid;
+}
+`
