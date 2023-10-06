db1c44c36882251906f27177af20c14e76fb9133
y00eunji <aine0227@naver.com>
2023-10-06 12:18:21 +0900

    fix: window 참조 에러 수정
    SL-122

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -3,16 +3,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Main from '@/components/Main/Main'
+import { useEffect } from 'react'
 
 export default function Home() {
-  let vh = window.innerHeight * 0.01
-
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-
-  window.addEventListener('resize', () => {
+  useEffect(() => {
     let vh = window.innerHeight * 0.01
+
     document.documentElement.style.setProperty('--vh', `${vh}px`)
-  })
+
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    })
+  }, [])
 
   return <Main />
 }
