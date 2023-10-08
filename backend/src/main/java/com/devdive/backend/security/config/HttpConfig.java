b62a58d87d3dd9b062f4abc3b14cb040d210e816
hunko <rhwlgns4386@naver.com>
2023-10-08 12:32:43 +0900

    feat : HttpSecurity 기본 필터 추가
    SL-184

--- backend/src/main/java/com/devdive/backend/security/config/HttpConfig.java
+++ backend/src/main/java/com/devdive/backend/security/config/HttpConfig.java
@@ -1,6 +1,9 @@
 package com.devdive.backend.security.config;
 
+import com.devdive.backend.auth.application.service.jwt.JwtProvider;
 import com.devdive.backend.security.config.builder.HttpSecurity;
+import com.devdive.backend.security.core.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -8,13 +11,29 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class HttpConfig {
 
+    @Autowired
+    private UserDataRepository userDataRepository;
+
+    @Autowired
+    private JwtProvider mailJwtProvider;
+
     @Bean
     @Scope(scopeName = "prototype")
     public HttpSecurity httpSecurity(){
-        HttpSecurity http = new HttpSecurity();
+        HttpSecurity http = new HttpSecurity(userDetailsService(loadMemberPort()));
         http.cors().anonymous().and()
-                .securityContext().and()
+                .securityContext().and().accessTokenValid().accessTokenProvider(mailJwtProvider).and()
                 .exceptionHandling();
         return http;
+    }
+
+    @Bean
+    public UserDetailsService<String> userDetailsService(LoadMemberPort loadMemberPort){
+        return new DefaultUserDetailsService<>(loadMemberPort);
+    }
+
+    @Bean
+    public LoadMemberPort loadMemberPort(){
+        return new LoadMemberPortImpl(userDataRepository);
     }
 }
