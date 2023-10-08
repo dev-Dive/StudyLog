175a6716a0bb0dcdc35a6ee1afcf12fd09d08270
hunko <rhwlgns4386@naver.com>
2023-10-08 12:33:31 +0900

    feat : FilterConfig 생성된 필터없으면 기본필터 추가 코드 추가
    SL-184

--- backend/src/main/java/com/devdive/backend/security/config/FilterConfig.java
+++ backend/src/main/java/com/devdive/backend/security/config/FilterConfig.java
@@ -1,7 +1,10 @@
 package com.devdive.backend.security.config;
 
+import com.devdive.backend.auth.application.service.jwt.JwtProvider;
+import com.devdive.backend.security.config.builder.HttpSecurity;
 import com.devdive.backend.security.filter.FilterChainProxy;
 import com.devdive.backend.security.filter.SecurityFilterChain;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
@@ -9,8 +12,28 @@ import java.util.List;
 @Configuration
 public class FilterConfig {
 
+    @Autowired
+    private JwtProvider mailJwtProvider;
+
+    @Autowired
+    private JwtProvider accessJwtProvider;
+
+    @Autowired
+    private JwtProvider refreshJwtProvider;
+
     @Bean(name = "securityFilter")
-    public FilterChainProxy filterChainProxy(List<SecurityFilterChain> securityFilterChains){
+    public FilterChainProxy filterChainProxy(HttpSecurity http, List<SecurityFilterChain> securityFilterChains){
+        if(securityFilterChains.size()==0){
+            http.tokenLogin()
+                    .mailProvider(mailJwtProvider)
+                    .accessTokenProvider(accessJwtProvider)
+                    .refreshTokenProvider(refreshJwtProvider)
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest()
+                    .permitAll();
+            securityFilterChains.add(http.build());
+        }
         return new FilterChainProxy(securityFilterChains);
     }
 }
