e16636d0acb16e46fed0881b382092892c3fc7be
hunko <rhwlgns4386@naver.com>
2023-10-08 12:45:43 +0900

    feat : AuthenticationCache 기능 추가
    SL-184

--- backend/src/main/java/com/devdive/backend/security/core/AuthenticationCache.java
+++ backend/src/main/java/com/devdive/backend/security/core/AuthenticationCache.java
@@ -4,4 +4,8 @@ public interface AuthenticationCache {
     Authentication findAuthentication(String email);
 
     void addAuthentication(String key, Authentication authentication);
+
+    void remove(String key);
+
+    void removeAll();
 }
