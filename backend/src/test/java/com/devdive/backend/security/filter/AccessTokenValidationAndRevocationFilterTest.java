cb7d61c9b0f75e8ab5a18956bc7acd7512e8e0d2
hunko <rhwlgns4386@naver.com>
2023-10-08 12:06:43 +0900

    test : AccessTokenValidationAndRevocationFilterTest 수정
    SL-184 exception을 잡아서 http상태코드를 반환하도록 테스트 변경

--- backend/src/test/java/com/devdive/backend/security/filter/AccessTokenValidationAndRevocationFilterTest.java
+++ backend/src/test/java/com/devdive/backend/security/filter/AccessTokenValidationAndRevocationFilterTest.java
@@ -1,26 +1,22 @@
 package com.devdive.backend.security.filter;
 
+import com.devdive.backend.auth.application.service.jwt.JwtProvider;
 import com.devdive.backend.security.core.Authentication;
 import com.devdive.backend.security.core.AuthenticationCache;
 import com.devdive.backend.security.core.SecurityContext;
 import com.devdive.backend.security.core.SecurityContextHolder;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
@@ -28,36 +24,29 @@ import static org.mockito.Mockito.*;
 
 public class AccessTokenValidationAndRevocationFilterTest {
 
+    private static final String secret = generateSecret();
+    private static final JwtProvider jwtProvider = new JwtProvider(secret, 1);
+
     @Test
     @DisplayName("필터 검증 성공 테스트")
     public void TokenValidationSuccessTest() throws ServletException, IOException {
-
-        //given
-        String secret = "devdive";
-
-        Date iat = new Date(System.currentTimeMillis());
-        String jwtToken = Jwts.builder()
-                .setHeader(new HashMap<>(Map.of("typ", "JWT", "alg", "HS256")))
-                .setSubject("test@gmail.com")
-                .setIssuedAt(iat)
-                .setExpiration(new Date(iat.getTime() + getHour(1)))
-                .signWith(SignatureAlgorithm.HS256, secret)
-                .compact();
+        String mail = "test@gmail.com";
+        String jwtToken = jwtProvider.createJwtToken(mail);
 
 
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader(HttpHeaders.AUTHORIZATION,"bearer "+ jwtToken);
+        request.addHeader(HttpHeaders.AUTHORIZATION, "bearer " + jwtToken);
         MockHttpServletResponse response = new MockHttpServletResponse();
         FilterChain filterChain = mock(FilterChain.class);
 
-        AuthenticationCache authenticationCache=mock(AuthenticationCache.class);
-        AccessTokenValidationAndRevocationFilter accessTokenValidationAndRevocationFilter = getAccessTokenValidationAndRevocationFilter(secret, authenticationCache);
+        AuthenticationCache authenticationCache = mock(AuthenticationCache.class);
+        AccessTokenValidationAndRevocationFilter accessTokenValidationAndRevocationFilter = getAccessTokenValidationAndRevocationFilter(authenticationCache);
 
         //when
-        accessTokenValidationAndRevocationFilter.doFilter(request,response,filterChain);
+        accessTokenValidationAndRevocationFilter.doFilter(request, response, filterChain);
 
         //then
-        verify(filterChain,times(1)).doFilter(eq(request),eq(response));
+        verify(filterChain, times(1)).doFilter(eq(request), eq(response));
     }
 
     @Test
@@ -65,30 +54,21 @@ public class AccessTokenValidationAndRevocationFilterTest {
     public void TokenValidationFailTest() throws ServletException, IOException {
 
         //given
-        String secret = "devdive";
-
-        Date iat = new Date(System.currentTimeMillis());
-        String jwtToken = Jwts.builder()
-                .setHeader(new HashMap<>(Map.of("typ", "JWT", "alg", "HS256")))
-                .setSubject("test@gmail.com")
-                .setIssuedAt(iat)
-                .setExpiration(new Date(iat.getTime() + getHour(1)))
-                .signWith(SignatureAlgorithm.HS256, secret+"s")
-                .compact();
+        String mail = "test@gmail.com";
+        String jwtToken = jwtProvider.createJwtToken(mail);
 
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader(HttpHeaders.AUTHORIZATION,"bearer "+ jwtToken);
+        request.addHeader(HttpHeaders.AUTHORIZATION, "bearer " + jwtToken + "aaaa");
         MockHttpServletResponse response = new MockHttpServletResponse();
         FilterChain filterChain = mock(FilterChain.class);
 
-        AuthenticationCache authenticationCache=mock(AuthenticationCache.class);
-        AccessTokenValidationAndRevocationFilter accessTokenValidationAndRevocationFilter = getAccessTokenValidationAndRevocationFilter(secret, authenticationCache);
+        AuthenticationCache authenticationCache = mock(AuthenticationCache.class);
+        AccessTokenValidationAndRevocationFilter accessTokenValidationAndRevocationFilter = getAccessTokenValidationAndRevocationFilter(authenticationCache);
+        accessTokenValidationAndRevocationFilter.doFilter(request, response, filterChain);
 
         //when
-        assertThatThrownBy(()->accessTokenValidationAndRevocationFilter.doFilter(request,response,filterChain))
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
 
-                //then
-                .isInstanceOf(SignatureException.class);
     }
 
     @Test
@@ -96,30 +76,21 @@ public class AccessTokenValidationAndRevocationFilterTest {
     public void tokenExpirationTest() throws ServletException, IOException {
 
         //given
-        String secret = "devdive";
-
-        Date iat = new Date(System.currentTimeMillis()-((getHour(1))+1));
-        String jwtToken = Jwts.builder()
-                .setHeader(new HashMap<>(Map.of("typ", "JWT", "alg", "HS256")))
-                .setSubject("test@gmail.com")
-                .setIssuedAt(iat)
-                .setExpiration(new Date(iat.getTime() + getHour(1)))
-                .signWith(SignatureAlgorithm.HS256, secret)
-                .compact();
+        String mail = "test@gmail.com";
+        JwtProvider jwtProvider1 = new JwtProvider(secret, 0);
+        String jwtToken = jwtProvider1.createJwtToken(mail);
 
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader(HttpHeaders.AUTHORIZATION,"bearer "+ jwtToken);
+        request.addHeader(HttpHeaders.AUTHORIZATION, "bearer " + jwtToken);
         MockHttpServletResponse response = new MockHttpServletResponse();
         FilterChain filterChain = mock(FilterChain.class);
 
-        AuthenticationCache authenticationCache=mock(AuthenticationCache.class);
-        AccessTokenValidationAndRevocationFilter accessTokenValidationAndRevocationFilter = getAccessTokenValidationAndRevocationFilter(secret, authenticationCache);
+        AuthenticationCache authenticationCache = mock(AuthenticationCache.class);
+        AccessTokenValidationAndRevocationFilter accessTokenValidationAndRevocationFilter = getAccessTokenValidationAndRevocationFilter(authenticationCache);
+        accessTokenValidationAndRevocationFilter.doFilter(request, response, filterChain);
 
         //when
-        assertThatThrownBy(()->accessTokenValidationAndRevocationFilter.doFilter(request,response,filterChain))
-
-                //then
-                .isInstanceOf(ExpiredJwtException.class);
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
     @Test
@@ -127,48 +98,34 @@ public class AccessTokenValidationAndRevocationFilterTest {
     public void SecurityContextSaveTest() throws ServletException, IOException {
 
         //given
-        String secret = "devdive";
-
-        Date iat = new Date(System.currentTimeMillis());
-        String email = "test@gmail.com";
-        String jwtToken = Jwts.builder()
-                .setHeader(new HashMap<>(Map.of("typ", "JWT", "alg", "HS256")))
-                .setSubject(email)
-                .setIssuedAt(iat)
-                .setExpiration(new Date(iat.getTime() + getHour(1)))
-                .signWith(SignatureAlgorithm.HS256, secret)
-                .compact();
+        String mail = "test@gmail.com";
+        String jwtToken = jwtProvider.createJwtToken(mail);
 
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader(HttpHeaders.AUTHORIZATION,"bearer "+ jwtToken);
+        request.addHeader(HttpHeaders.AUTHORIZATION, "bearer " + jwtToken);
         MockHttpServletResponse response = new MockHttpServletResponse();
         FilterChain filterChain = mock(FilterChain.class);
 
-        AuthenticationCache authenticationCache=mock(AuthenticationCache.class);
-        when(authenticationCache.findAuthentication(eq(email))).thenReturn(mock(Authentication.class));
+        AuthenticationCache authenticationCache = mock(AuthenticationCache.class);
+        when(authenticationCache.findAuthentication(eq(mail))).thenReturn(mock(Authentication.class));
 
-        AccessTokenValidationAndRevocationFilter accessTokenValidationAndRevocationFilter = getAccessTokenValidationAndRevocationFilter(secret, authenticationCache);
+        AccessTokenValidationAndRevocationFilter accessTokenValidationAndRevocationFilter = getAccessTokenValidationAndRevocationFilter(authenticationCache);
 
-        try(MockedStatic<SecurityContextHolder> mockContextHolder = mockStatic(SecurityContextHolder.class)) {
+        try (MockedStatic<SecurityContextHolder> mockContextHolder = mockStatic(SecurityContextHolder.class)) {
             TestSecurityContext testSecurityContext = new TestSecurityContext();
             mockContextHolder.when(SecurityContextHolder::getContext).thenReturn(testSecurityContext);
 
             //when
-            accessTokenValidationAndRevocationFilter.doFilter(request,response,filterChain);
+            accessTokenValidationAndRevocationFilter.doFilter(request, response, filterChain);
 
             //then
-            mockContextHolder.verify(SecurityContextHolder::getContext,times(1));
+            mockContextHolder.verify(SecurityContextHolder::getContext, times(1));
             assertThat(testSecurityContext.getAuthentication()).isNotNull();
         }
     }
 
-    private static AccessTokenValidationAndRevocationFilter getAccessTokenValidationAndRevocationFilter(String secret, AuthenticationCache authenticationCache) {
-        AccessTokenValidationAndRevocationFilter accessTokenValidationAndRevocationFilter=new AccessTokenValidationAndRevocationFilter(secret, authenticationCache);
-        return accessTokenValidationAndRevocationFilter;
-    }
-
-    private static int getHour(int hour) {
-        return 1_000 * 60 * 60 * hour;
+    private static AccessTokenValidationAndRevocationFilter getAccessTokenValidationAndRevocationFilter(AuthenticationCache authenticationCache) {
+        return new AccessTokenValidationAndRevocationFilter(jwtProvider, authenticationCache);
     }
 
     static class TestSecurityContext implements SecurityContext {
@@ -182,7 +139,15 @@ public class AccessTokenValidationAndRevocationFilterTest {
 
         @Override
         public void setAuthentication(Authentication authentication) {
-            this.authentication=authentication;
+            this.authentication = authentication;
         }
+    }
+
+    private static String generateSecret() {
+        Random random = new Random();
+        return random.ints('0', 'z')
+                .filter(value -> ('0' <= value && value <= '9') || ('A' <= value && value <= 'Z') || ('a' <= value && value <= 'z'))
+                .limit((255 / 2) + 1).collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 }
