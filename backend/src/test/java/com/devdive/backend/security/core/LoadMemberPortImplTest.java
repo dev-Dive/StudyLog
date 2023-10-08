1846dcae9585c088473e11c9e8e9423fee696247
hunko <rhwlgns4386@naver.com>
2023-10-08 12:20:14 +0900

    test : LoadMemberPortImpl 테스트 작성
    SL-184

---
+++ backend/src/test/java/com/devdive/backend/security/core/LoadMemberPortImplTest.java
@@ -0,0 +1,29 @@
+package com.devdive.backend.security.core;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.devdive.backend.security.core.LoadMemberPort.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class LoadMemberPortImplTest {
+
+    @Test
+    @DisplayName("이메일로 사용자 검색 테스트")
+    void findByEmailTest() {
+        String mail = "rkdtjdqja@qkqh.com";
+        UserDataRepository userDataRepository = mock(UserDataRepository.class);
+        when(userDataRepository.findByMail(eq(mail))).thenReturn(new UserData(1L, "rkdtjdqja@qkqh.com"));
+
+        LoadMemberPortImpl loadMemberPort = new LoadMemberPortImpl(userDataRepository);
+
+        UserData findData = loadMemberPort.findByEmail(mail);
+
+        assertThat(findData).isNotNull();
+        assertThat(findData.getId()).isEqualTo(1);
+        assertThat(findData.getMail()).isEqualTo("rkdtjdqja@qkqh.com");
+    }
+}
