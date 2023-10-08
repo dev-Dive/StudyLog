fe5ba20b9fd483ca83299e4a7b3973d86fc122fb
hunko <rhwlgns4386@naver.com>
2023-10-08 12:15:53 +0900

    test : InMemoryAuthenticationCache 테스트 작성
    SL-184

---
+++ backend/src/test/java/com/devdive/backend/security/core/InMemoryAuthenticationCacheTest.java
@@ -0,0 +1,34 @@
+package com.devdive.backend.security.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class InMemoryAuthenticationCacheTest {
+    AuthenticationCache inMemoryAuthenticationCache = InMemoryAuthenticationCache.getInstance();
+
+    @Test
+    void findAuthentication() {
+        String mail = "test@mail.com";
+        Authentication authentication = mock(Authentication.class);
+        inMemoryAuthenticationCache.addAuthentication(mail, authentication);
+        Authentication findAuthentication = inMemoryAuthenticationCache.findAuthentication(mail);
+
+        assertThat(findAuthentication).isEqualTo(authentication);
+    }
+
+    @Test
+    void addAuthentication() {
+        String mail = "test@mail.com";
+        Authentication authentication1 = mock(Authentication.class);
+        Authentication authentication2 = mock(Authentication.class);
+        Authentication authentication3 = mock(Authentication.class);
+        inMemoryAuthenticationCache.addAuthentication(mail, authentication1);
+        inMemoryAuthenticationCache.addAuthentication(mail, authentication2);
+        inMemoryAuthenticationCache.addAuthentication(mail, authentication3);
+        Authentication findAuthentication = inMemoryAuthenticationCache.findAuthentication(mail);
+
+        assertThat(findAuthentication).isEqualTo(authentication3);
+    }
+}
