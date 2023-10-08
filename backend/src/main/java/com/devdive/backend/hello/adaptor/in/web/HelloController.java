bdf65bd7bb2ad8a3b9a48581dc962904e77e0f45
hunko <rhwlgns4386@naver.com>
2023-10-08 12:44:59 +0900

    feat : HelloController 기능 추가
    SL-184

--- backend/src/main/java/com/devdive/backend/hello/adaptor/in/web/HelloController.java
+++ backend/src/main/java/com/devdive/backend/hello/adaptor/in/web/HelloController.java
@@ -1,17 +1,26 @@
 package com.devdive.backend.hello.adaptor.in.web;
 
+import com.devdive.backend.security.core.AuthenticationPrincipal;
+import com.devdive.backend.security.core.UserDetails;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("hello");
+    }
+
+    @GetMapping("/hello/admin")
+    public ResponseEntity<String> admin(@AuthenticationPrincipal UserDetails user) {
+        return ResponseEntity.ok(user.getUsername());
     }
 
 }
