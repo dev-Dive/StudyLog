2f60eea1e0e856dea3c6cc8ac019add60dcff932
hunko <rhwlgns4386@naver.com>
2023-10-08 12:28:40 +0900

    feat : 토큰 인증필터 및 로그인필터 컴피거 추가
    SL-184

---
+++ backend/src/main/java/com/devdive/backend/security/config/builder/configur/AccessTokenValidConfigurer.java
@@ -0,0 +1,45 @@
+package com.devdive.backend.security.config.builder.configur;
+
+import com.devdive.backend.auth.application.service.jwt.JwtProvider;
+import com.devdive.backend.security.config.builder.HttpSecurity;
+import com.devdive.backend.security.core.AuthenticationCache;
+import com.devdive.backend.security.core.InMemoryAuthenticationCache;
+import com.devdive.backend.security.filter.AccessTokenValidationAndRevocationFilter;
+
+public class AccessTokenValidConfigurer extends AbstractSecurityConfigurer {
+
+    private JwtProvider jwtProvider;
+    private AuthenticationCache authenticationCache;
+
+    @Override
+    public void init() {
+        if (jwtProvider == null) {
+            throw new IllegalStateException("jwtProvider must be not null");
+        }
+
+        if (authenticationCache == null) {
+            authenticationCache = InMemoryAuthenticationCache.getInstance();
+        }
+    }
+
+    @Override
+    public void config(HttpSecurity http) {
+        http.addFilter(new AccessTokenValidationAndRevocationFilter(jwtProvider, authenticationCache));
+    }
+
+
+    public AccessTokenValidConfigurer accessTokenProvider(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+        return this;
+    }
+
+    public AccessTokenValidConfigurer accessTokenProvider(String secret, int hour) {
+        this.jwtProvider = new JwtProvider(secret, hour);
+        return this;
+    }
+
+    public AccessTokenValidConfigurer changeCache(AuthenticationCache authenticationCache) {
+        this.authenticationCache = authenticationCache;
+        return this;
+    }
+}
---
+++ backend/src/main/java/com/devdive/backend/security/config/builder/configur/TokenLoginConfigurer.java
@@ -0,0 +1,76 @@
+package com.devdive.backend.security.config.builder.configur;
+
+import com.devdive.backend.auth.application.service.jwt.JwtProvider;
+import com.devdive.backend.security.config.builder.HttpSecurity;
+import com.devdive.backend.security.core.*;
+import com.devdive.backend.security.filter.TokenAuthenticationFilter;
+
+public class TokenLoginConfigurer extends AbstractSecurityConfigurer {
+
+    private UserDetailsService<String> userDetailsService;
+    private JwtProvider mailJwtProvider;
+    private JwtProvider accessJwtProvider;
+    private JwtProvider refreshJwtProvider;
+
+    private String patternUrl = "/api/v1/auth/mail/login";
+
+    private AuthenticationCache authenticationCache;
+
+    @Override
+    public void init() {
+
+        if (mailJwtProvider == null) {
+            throw new IllegalStateException("mailProvider must be not null");
+        }
+
+        if (accessJwtProvider == null) {
+            throw new IllegalStateException("accessProvider must be not null");
+        }
+
+        if (refreshJwtProvider == null) {
+            throw new IllegalStateException("refreshProvider must be not null");
+        }
+
+        if (authenticationCache == null) {
+            authenticationCache = InMemoryAuthenticationCache.getInstance();
+        }
+    }
+
+    @Override
+    public void config(HttpSecurity http) {
+        TokenAuthenticationFilter tokenAuthenticationFilter
+                = new TokenAuthenticationFilter(userDetailsService
+                , mailJwtProvider
+                , accessJwtProvider
+                , refreshJwtProvider
+                , patternUrl
+                , authenticationCache);
+
+        http.addFilter(tokenAuthenticationFilter);
+    }
+
+    public TokenLoginConfigurer mvcMatchers(String patternUrl) {
+        this.patternUrl = patternUrl;
+        return this;
+    }
+
+    public TokenLoginConfigurer mailProvider(JwtProvider jwtProvider) {
+        this.mailJwtProvider = jwtProvider;
+        return this;
+    }
+
+    public TokenLoginConfigurer accessTokenProvider(JwtProvider jwtProvider) {
+        this.accessJwtProvider = jwtProvider;
+        return this;
+    }
+
+    public TokenLoginConfigurer refreshTokenProvider(JwtProvider jwtProvider) {
+        this.refreshJwtProvider = jwtProvider;
+        return this;
+    }
+
+    public TokenLoginConfigurer setUserDetailService(UserDetailsService<String> userDetailsService) {
+        this.userDetailsService = userDetailsService;
+        return this;
+    }
+}
