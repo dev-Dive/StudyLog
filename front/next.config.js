86ec8816f3bbc8f430dcd68563a301253564cc64
y00eunji <aine0227@naver.com>
2023-10-24 21:13:49 +0900

    chore: 패키지 변경
    SL-126

--- front/next.config.js
+++ front/next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/login',
-        destination: '/?type=login',
-        permanent: false,
-      },
-      {
-        source: '/register',
-        destination: '/?type=register',
-        permanent: false,
-      },
-    ]
+  compiler: {
+    emotion: true,
   },
 }
 
